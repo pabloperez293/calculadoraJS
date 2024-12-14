a92e722fb267ecaa50322613e35e541742755f58
Pablo Perez <paperezpablo@gmail.com>
2024-12-14 01:19:09 -0300

    metods

--- 1-Calculadora/js/main.js
+++ 1-Calculadora/js/main.js
@@ -2,5 +2,19 @@ const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
-    item.onclick
-})
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backSpace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval();
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerHTML = "Null";
+      setTimeout(() => ((display.innerText = ""), 2000));
+    } else {
+        display.innerText += item.id;
+    }
+  };
+});
