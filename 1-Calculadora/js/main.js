313c2f0c86edbc134475f32c223538563e6f95f6
Pablo Perez <paperezpablo@gmail.com>
2025-01-08 11:08:41 -0300

    Seg

--- 1-Calculadora/js/main.js
+++ 1-Calculadora/js/main.js
@@ -2,7 +2,16 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
+// Secure function for evaluating mathematical expressions
+const safeEval = (expression) => {
+  try {
+    
+    return Function('"use strict"; return (' + expression + ')')();
+  } catch {
+    return "Error"; 
+};
 // Iterate over each button to assign events
+
 buttons.forEach((item) => {
   item.onclick = () => {
     if (item.id == "clear") {
@@ -11,10 +20,10 @@ buttons.forEach((item) => {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
     } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
+      display.innerText = safeEval(display.innerText);
     } else if (display.innerText == "" && item.id == "equal") {
       display.innerHTML = "Null";
-      setTimeout(() => ((display.innerText = ""), 2000));
+      setTimeout(() => (display.innerText = ""), 2000);
     } else {
       display.innerText += item.id;
     }
@@ -28,7 +37,9 @@ const calculator = document.querySelector(".calculator");
 const togglerIcon = document.querySelector(".fa-solid");
 
 let isDark = true;
+
 // Event to toggle theme
+
 themeToggleBtn.onclick = () => {
   calculator.classList.toggle("dark");
   themeToggleBtn.classList.toggle("active");
